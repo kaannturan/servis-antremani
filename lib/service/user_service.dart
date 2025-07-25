e6a351cdc61fca0d602151321197d444a355d0eb
kaannturan <kaannturan@gmail.com>
2025-07-25 15:16:23 +0300

    son

--- lib/service/user_service.dart
+++ lib/service/user_service.dart
@@ -3,14 +3,21 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:rastgele_antreman/models/user_model.dart';
 
+// istek atacağımız url'yi belirledik ve .get metodu ile istediğimizi attık,
+// burada baseUrl: ana api adresi, endpoint ise istek yapılacak bölüm.
 class UserService {
   final String baseUrl = "https://reqres.in/api/";
   final String endpoint = "users?page=2";
 
   Future<UserModel?> fetchUser() async {
+    // gelen veriyi Uri.parse metodu ile flutter'ın anlayacağı biçime çeviriyoruz.
+    // ve veriyi res değişkeninine setliyoruz
     try {
+      // res'e gelen veri : json verisi
       final res = await http.get(Uri.parse("$baseUrl$endpoint"));
       if (res.statusCode == 200) {
+        // sonrasında gelen veri UserModel objesine dönüştüğü için dart objesine çeviriyoruz ve
+        // jsonBody değişkeninde tutuyoruz
         var jsonBody = UserModel.fromJson(json.decode(res.body));
         return jsonBody;
       } else {
